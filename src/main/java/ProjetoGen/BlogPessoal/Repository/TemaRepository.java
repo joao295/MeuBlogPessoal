782cde63057442fe5175fb6d002e0444ec05a57a
João Almeida <78218571+joao295@users.noreply.github.com>
2022-01-04 09:00:37 -0300

    @joao295 [Task: add @repository in TemaRepository]

--- src/main/java/ProjetoGen/BlogPessoal/Repository/TemaRepository.java
+++ src/main/java/ProjetoGen/BlogPessoal/Repository/TemaRepository.java
@@ -8,6 +8,7 @@ package ProjetoGen.BlogPessoal.Repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import ProjetoGen.BlogPessoal.Model.Tema;
 
@@ -17,7 +18,7 @@ import ProjetoGen.BlogPessoal.Model.Tema;
 //* extends JpaRepository ele pega todos os metodos crud 
 
 //*
-
+@Repository
 public interface TemaRepository extends JpaRepository <Tema, Long> {
 
 		public List<Tema> findAllByDescricaoContainingIgnoreCase (String descricao);
