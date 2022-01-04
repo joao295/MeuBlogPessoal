f8f7ccda152695e13dff621f5e8089208e06c113
João Almeida <78218571+joao295@users.noreply.github.com>
2022-01-04 09:01:12 -0300

    @joao295 [Task: add @repository in UsuarioRepository]

--- src/main/java/ProjetoGen/BlogPessoal/Repository/UsuarioRepository.java
+++ src/main/java/ProjetoGen/BlogPessoal/Repository/UsuarioRepository.java
@@ -4,11 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import ProjetoGen.BlogPessoal.Model.Usuario;
 
 
-
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 	 public Optional<Usuario> findByUsuario(String usuario);
